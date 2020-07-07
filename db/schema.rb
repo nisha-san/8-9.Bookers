12252e0ae7e1e0a2165b0503b662aaf8d04c743a
s-kawasaki <s-kawasaki@cherpa.co.jp>
2020-07-07 21:32:46 +0900

    bookモデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_101216) do
+ActiveRecord::Schema.define(version: 2020_07_07_122336) do
 
   create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
