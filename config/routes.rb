6cf5d1766a801fa3fd895db0316f62bedda60f48
s-kawasaki <s-kawasaki@cherpa.co.jp>
2020-07-08 01:21:37 +0900

    各ページへのリンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'homes#index'
+  get 'home/about' => 'homes#about'
+  get 'books' => 'books#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users' => 'users#show'
 end
